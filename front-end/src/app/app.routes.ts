d69159eadb3b99947c1f5bf7bd591e07cf35ceda
alejandro lafuente <alejandromopi@hotmail.com>
2024-09-20 23:02:56 -0300

    deleting front-end

--- front-end/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginRoutes } from './authentication/authentication-routing.module';
-
-export const routes: Routes = [
-    ...LoginRoutes
-];
--- front-end/src/app/shared/models/loginObject/login-object.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LoginObject } from './login-object.model';
-
-describe('LoginObject', () => {
-  it('should create an instance', () => {
-    expect(new LoginObject()).toBeTruthy();
-  });
-});
