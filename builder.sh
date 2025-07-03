bd8540a0fa78720e6fd7e5bfbe09c312f31ef34e
alejandro lafuente <alejandromopi@hotmail.com>
2025-07-03 18:48:49 -0300

    commit de update

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# esta sendo construido, nao usar este script!
 chmod +x builder.sh
 
 docker network create sagas-network
@@ -16,54 +16,71 @@ docker network create command-network
 
 docker network create query-network
 
-cd back-end/broker-rabbitMQ
+# Função para encontrar porta livre
+find_free_port() {
+    local base_port=$1
+    while (netstat -tuln | grep -q ":${base_port} ") || (ss -tuln | grep -q ":${base_port} "); do
+        ((base_port++))
+    done
+    echo $base_port
+}
 
-docker-compose up -d
+# Define portas padrão ou alternativas
+export RABBIT_PORT=$(find_free_port 5672)
+export RABBIT_MGMT_PORT=$(find_free_port 15672)
 
-cd ..
+echo "Usando portas: RabbitMQ AMQP=${RABBIT_PORT}, Management=${RABBIT_MGMT_PORT}"
 
-cd sagas
+# cd back-end/broker-rabbitMQ
 
-docker compose up -d --build # --build força a reconstrução da imagem
+# docker-compose up -d
 
-cd ..
-
-cd authentication-service
-
-docker-compose up -d --build
-
-cd ..
-
-cd client-service
-
-docker-compose up -d --build
-
-cd ..
-
-cd flight-service
-
-docker-compose up -d --build
+docker-compose -f back-end/broker-rabbitMQ/docker-compose.yml up -d
 
 cd ..
 
-cd employee-service
+# cd sagas
 
-docker-compose up -d --build
+# docker compose up -d --build # --build força a reconstrução da imagem
 
-cd ..
+# cd ..
 
-cd booking-command-service
+# cd authentication-service
 
-docker-compose up -d --build
+# docker-compose up -d --build
 
-cd ..
+# cd ..
 
-cd booking-query-service
+# cd client-service
 
-docker-compose up -d --build
+# docker-compose up -d --build
 
-cd ..
+# cd ..
 
-cd pgadmin
+# cd flight-service
 
-docker-compose up -d
+# docker-compose up -d --build
+
+# cd ..
+
+# cd employee-service
+
+# docker-compose up -d --build
+
+# cd ..
+
+# cd booking-command-service
+
+# docker-compose up -d --build
+
+# cd ..
+
+# cd booking-query-service
+
+# docker-compose up -d --build
+
+# cd ..
+
+# cd pgadmin
+
+# docker-compose up -d
